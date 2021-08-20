64f40c7225ff35f0e370db74bb93488cea025732
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-08-20 03:07:23 +0000

    add count of categories and products to pass to views

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with :name => ENV['HTTP_BASIC_AUTH_USER'], :password => ENV['HTTP_BASIC_AUTH_PASSWORD']
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
 
   def show
+    @total_products = Product.count
+    @total_categories = Category.count
   end
+
 end
