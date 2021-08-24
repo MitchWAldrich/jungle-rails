56a7cbc520b0c4f3152f5ee603600912fa7aa764
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-08-24 18:51:35 +0000

    add authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ActiveRecord::Base
-end
-class User < ActiveRecord::Base
   has_secure_password
+  
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
+  
+  def self.authenticate_with_credentials(user_email, user_password)
+    @user = User.find_by email: user_email 
+    if @user && @user.authenticate(user_password)
+      @user
+    else
+      nil
+    end
+  end
+
 end
